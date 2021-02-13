9cd1f91c2ff73a0a62317da00401f43c543529db
olvinpineda <olvin_xavier06@hotmail.com>
2021-02-13 16:07:09 -0600

    Agregando arquitectura basica

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductoController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductoController.cs
@@ -1,4 +1,5 @@
-﻿using QuieroPizza.Web.Models;
+﻿
+using QuieroPizza.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +13,9 @@ namespace QuieroPizza.Web.Controllers
         // GET: Producto
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza 6  quesos";
 
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pizza 4 Estaciones";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pizza Jamon y Queso";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-                
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
                 
             return View(listadeProductos);
         }
